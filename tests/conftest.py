9ae7a15ac078ee0be1c0bd5799fcfdb9d3c0b738
Jakub Hejhal <kubic71@gmail.com>
2023-07-07 23:55:53 +0200

    wip

---
+++ tests/conftest.py
@@ -0,0 +1,25 @@
+import pytest
+
+@pytest.fixture(scope="session")
+def owner(accounts):
+    return accounts[0]
+
+@pytest.fixture(scope="session")
+def countdownPeriod():
+    return 600
+
+@pytest.fixture(scope="session")
+def threshold():
+    return 1000000000000000000
+
+@pytest.fixture(scope="session")
+def receiver(accounts):
+    return accounts[1]
+
+@pytest.fixture(scope='session', autouse=True)
+def vowelsounds_contract(project, owner, countdownPeriod, threshold, receiver):
+    vowelsounds_contracts_dependency_api = project.dependencies["contribution-contracts"]
+    # simply use first entry - could be from github ('main') or local ('local')
+    _, vowelsounds_contracts = list(vowelsounds_contracts_dependency_api.items())[0]
+    compiled = vowelsounds_contracts.compile()
+    return owner.deploy(vowelsounds_contracts.Contribution, countdownPeriod, threshold, receiver)
---
+++ tests/test_main.py
@@ -0,0 +1,3 @@
+
+def test_dummy(vowelsounds_contract):
+    print(vowelsounds_contract)
