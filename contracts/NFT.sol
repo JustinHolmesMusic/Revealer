cd8ff1e5102efcf74a07ea2ffb3374f786efd997
Kieran Prasch <kieranprasch@gmail.com>
2023-07-09 01:36:43 +0200

    more contract hacking

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -6,13 +6,51 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract VowelSoundsNFT is ERC721URIStorage, Ownable {
     uint256 private _tokenIdCounter;
-    mapping(address => bool) public members;
+    address payable public admin;
+    bool isEnrollmentOpen = true;
+    mapping(address => uint) public members;
     mapping(address => bool) private _hasMinted;
 
-    constructor() ERC721("VowelSoundsNFT", "AEIOU") {}
+    constructor(
+        address payable _amb
+    ) ERC721("VowelSoundsNFT", "AEIOU") {
+        amb = address(_amb);
+    }
 
-    function addMember(address member) public onlyOwner {
-        members[member] = true;
+    function addMember(address member, uint value) external {
+        require(msg.sender == admin, "Only amb can add members");
+        members[member] = value;
+    }
+
+    function endEnrollment() external {
+        require(msg.sender == admin, "Only amb can end enrollment");
+        isEnrollmentOpen = false;
+    }
+
+    // sort members by value in descending order
+    function sortMembers() external {
+        require(!isEnrollmentOpen, "Enrollment must be closed before sorting");
+        uint n = 0;
+        address[] memory keys = new address[](members.length);
+        for (uint i = 0; i < members.length; i++) {
+            keys[i] = members[i];
+        }
+        for (uint i = 0; i < members.length; i++) {
+            for (uint j = i + 1; j < members.length; j++) {
+                if (members[keys[i]] < members[keys[j]]) {
+                    address temp = keys[i];
+                    keys[i] = keys[j];
+                    keys[j] = temp;
+                }
+            }
+        }
+        for (uint i = 0; i < members.length; i++) {
+            members[keys[i]] = i;
+        }
+    }
+
+    function setAdmin(address payable _admin) external onlyOwner {
+        admin = _admin;
     }
 
     function mintNFT() public {
