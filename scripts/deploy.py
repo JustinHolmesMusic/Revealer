730472920654c16c7e84921749d032a6a1dfc849
Kieran Prasch <kieranprasch@gmail.com>
2023-07-09 01:17:36 +0200

    updates deployment script

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 from ape import project
 from ape.cli import get_user_selected_account
+from web3 import Web3
+
 
 def main(account_id=None):
     deployer = get_user_selected_account()
 
-    coordinator = project.Contribution.deploy(
-        1687669457,  # June 25th
+    coordinator = project.TestnetContribution.deploy(
+        120,
+        Web3.to_wei(1, 'ether'),
+        deployer,
         sender=deployer,
         publish=True,
     )
