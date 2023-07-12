b3f341e72aed4824bacb1e3ddd0b3d24605a5f0b
jMyles <justin@justinholmes.com>
2023-07-12 14:03:03 +0200

    Begining of network status response.

--- revealer_bot/message_parser.py
+++ revealer_bot/message_parser.py
@@ -1,11 +1,23 @@
 from bot_responses import SimpleReply
-from revealer_bot.decryption_action import decrypt_attached_tmk
 from built_in_messages import simple_reply_defaults
-
+from revealer_bot.bot_responses import BotActionResponse
+from bob_and_other_networky_things import bob
 
 available_responses = list()
 for prompt, reply in simple_reply_defaults:
     available_responses.append(SimpleReply(triggers=(prompt,), initial_reply=reply))
+
+
+def network_status(message):
+    assert False
+
+
+network_status_response = BotActionResponse(action=network_status,
+                                            must_mention=True,
+                                            triggers=("network status",),
+                                            initial_reply="Network status")
+
+available_responses.append(network_status_response)
 
 
 async def parse_message(message):
@@ -13,4 +25,3 @@ async def parse_message(message):
         for trigger in candidate_response.triggers:
             if trigger in message.content.lower():
                 await candidate_response.respond_to(message)
-
