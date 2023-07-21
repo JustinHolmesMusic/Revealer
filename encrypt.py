d2bd877d03a916a14c9b02384e39c474be7f7539
Jakub Hejhal <kubic71@gmail.com>
2023-07-22 00:49:00 +0200

    cleanup and refactor encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -1,13 +1,16 @@
+import argparse
 import base64
 import json
+from pathlib import Path
 
-from nucypher_core.ferveo import DkgPublicKey
-
+from cryptography.fernet import Fernet
+from eth_utils import keccak # type: ignore
 from nucypher.blockchain.eth.agents import CoordinatorAgent
 from nucypher.blockchain.eth.registry import InMemoryContractRegistry
 from nucypher.characters.lawful import Enrico
-from nucypher.policy.conditions.lingo import ConditionLingo
+from nucypher.policy.conditions.lingo import ConditionLingo, Lingo
 from nucypher.utilities.logging import GlobalLoggerSettings
+from nucypher_core.ferveo import DkgPublicKey
 
 ######################
 # Boring setup stuff #
@@ -17,90 +20,100 @@ LOG_LEVEL = "info"
 GlobalLoggerSettings.set_log_level(log_level_name=LOG_LEVEL)
 GlobalLoggerSettings.start_console_logging()
 
-staking_provider_uri = 
-network = "lynx"
-
-coordinator_provider_uri = 
-coordinator_network = "mumbai"
-
 #####################
 # Scully the Symmet
 #####################
-from cryptography.fernet import Fernet
-from eth_utils import keccak
 
-def keygen():
+
+def keygen() -> bytes:
     _secret = Fernet.generate_key()
     return _secret
 
-with open('manzana.mp3', 'rb') as tony:
-    definitely_tony = tony.read()
 
-def encapsulate(secret):
+def encapsulate(secret: bytes, clear_text: bytes) -> bytes:
     f = Fernet(secret)
-    capsule = f.encrypt(definitely_tony)
+    capsule = f.encrypt(clear_text)
     return capsule
 
-plaintext_of_sym_key = keygen()
-secret_hash = keccak(plaintext_of_sym_key)
-bulk_ciphertext = encapsulate(plaintext_of_sym_key)
 
-###############
-# Enrico
-###############
+def main(args):
+    file_path = Path(args.file_path)
 
-print("--------- Threshold Encryption ---------")
+    with open(file_path, "rb") as f:
+        clear_text = f.read()
 
-coordinator_agent = CoordinatorAgent(
-    provider_uri=coordinator_provider_uri,
-    registry=InMemoryContractRegistry.from_latest_publication(
-        network=coordinator_network
-    ),
-)
-ritual_id = 15  # got this from a side channel
-ritual = coordinator_agent.get_ritual(ritual_id)
-enrico = Enrico(encrypting_key=DkgPublicKey.from_bytes(bytes(ritual.public_key)))
+    plaintext_of_sym_key = keygen()
 
-print(
-    f"Fetched DKG public key {bytes(enrico.policy_pubkey).hex()} "
-    f"for ritual #{ritual_id} "
-    f"from Coordinator {coordinator_agent.contract.address}"
-)
+    # TODO: what do we do with this secret hash?
+    # It's not used anywhere atm.
+    secret_hash = keccak(plaintext_of_sym_key)
+    bulk_ciphertext = encapsulate(plaintext_of_sym_key, clear_text)
 
-eth_balance_condition = {
-    "version": ConditionLingo.VERSION,
-    "condition": {
-        "chain": 80001,
-        "method": "eth_getBalance",
-        "parameters": ["0x210eeAC07542F815ebB6FD6689637D8cA2689392", "latest"],
-        "returnValueTest": {"comparator": "==", "value": 0},
-    },
-}
+    print("--------- Threshold Encryption ---------")
 
-ciphertext_of_sym_key = enrico.encrypt_for_dkg(plaintext=plaintext_of_sym_key,
-                                               conditions=eth_balance_condition)
+    coordinator_agent = CoordinatorAgent(
+        provider_uri=args.coordinator_provider_uri,
+        registry=InMemoryContractRegistry.from_latest_publication(network=args.coordinator_network),
+    )
+    ritual_id = args.ritual_id
+    ritual = coordinator_agent.get_ritual(ritual_id)
+    enrico = Enrico(encrypting_key=DkgPublicKey.from_bytes(bytes(ritual.public_key)))
 
-tmk = {
-    'bulk_ciphertext': base64.b64encode(bytes(bulk_ciphertext)).decode(),  # Encrypted Tony
-    'encrypted_sym_key': bytes(ciphertext_of_sym_key).hex(),
-    'conditions': eth_balance_condition,
-    'filename': 'manzana.mp3'
-}
+    print(
+        f"Fetched DKG public key {bytes(enrico.policy_pubkey).hex()} " # type: ignore
+        f"for ritual #{ritual_id} "
+        f"from Coordinator {coordinator_agent.contract.address}"
+    )
 
-################
-# Sanity check #
-################
+    eth_balance_condition: Lingo = {
+            "version": ConditionLingo.VERSION,
+            "condition": {
+                "chain": args.chain,
+                "method": "eth_getBalance",
+                "parameters": [args.eth_address, "latest"],
+                "returnValueTest": {"comparator": "==", "value": 0},
+            },
+        }
 
-f = Fernet(plaintext_of_sym_key)
-hopefully_tony = f.decrypt(bulk_ciphertext)
-assert hopefully_tony == definitely_tony
+    ciphertext_of_sym_key = enrico.encrypt_for_dkg(
+        plaintext=plaintext_of_sym_key, conditions=eth_balance_condition
+    )
 
-##################
+    tmk = {
+        "bulk_ciphertext": base64.b64encode(bytes(bulk_ciphertext)).decode(),  # Encrypted Tony
+        "encrypted_sym_key": bytes(ciphertext_of_sym_key).hex(),
+        "conditions": eth_balance_condition,
+        "filename": file_path.name,
+    }
 
-tmk_json = json.dumps(tmk)
+    ################
+    # Sanity check #
+    ################
 
-filename = 'tony.tmk'
-with open(filename, 'w') as file:
-    data = tmk_json
-    file.write(data)
-    print(f'Wrote {len(data)} bytes to {filename}')
+    f = Fernet(plaintext_of_sym_key)
+    hopefully_decrypted = f.decrypt(bulk_ciphertext)
+    assert hopefully_decrypted == clear_text
+
+    ##################
+
+    tmk_json = json.dumps(tmk)
+
+    filename = args.output_file
+    with open(filename, "w") as file:
+        data = tmk_json
+        file.write(data)
+        print(f"Wrote {len(data)} bytes to {filename}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Encrypt file using threshold encryption.")
+    parser.add_argument("--file_path", type=str, default="manzana.mp3", help="Path to the file to be encrypted")
+    parser.add_argument("--ritual_id", type=int, help="Ritual ID obtained from a side channel", default=15)
+    parser.add_argument("--coordinator_provider_uri", type=str, help="URI of the coordinator provider", required=True)
+    parser.add_argument("--coordinator_network", type=str, default="mumbai", help="Network for the coordinator", choices=["mumbai", "rinkeby", "mainnet", "goerli", "ropsten", "kovan"])
+    parser.add_argument("--chain", type=int, help="Ethereum chain ID", default=80001)
+    parser.add_argument("--eth_address", type=str, help="Ethereum address for balance check", default="0x210eeAC07542F815ebB6FD6689637D8cA2689392")
+    parser.add_argument("--output-file", type=str, default="tony.tmk", help="Output file for encrypted data")
+
+    args = parser.parse_args()
+    main(args)
