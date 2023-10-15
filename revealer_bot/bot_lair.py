6780e6308544aa5cce86632833d080d48457fa38
jMyles <justin@justinholmes.com>
2023-10-15 15:26:54 -0700

    Guild ID for Threshold.

--- revealer_bot/bot_lair.py
+++ revealer_bot/bot_lair.py
@@ -1,7 +1,8 @@
 import discord
 from discord import app_commands
 
-JM_MUSIC_GUILD_ID = discord.Object(id=1126841404056948806)  # replace with your guild id
+JM_MUSIC_GUILD_ID = discord.Object(id=1126841404056948806)
+THRESHOLD_GUILD_ID = discord.Object(id=866378471868727316)
 
 
 class RevealerBotDiscordClient(discord.Client):
@@ -10,10 +11,11 @@ class RevealerBotDiscordClient(discord.Client):
         self.tree = app_commands.CommandTree(self)
 
     async def setup_hook(self):
-        # This copies the global commands over to your guild.
         self.tree.copy_global_to(guild=JM_MUSIC_GUILD_ID)
-        await self.tree.sync(guild=JM_MUSIC_GUILD_ID)
+        self.tree.copy_global_to(guild=THRESHOLD_GUILD_ID)
 
+        await self.tree.sync(guild=JM_MUSIC_GUILD_ID)
+        await self.tree.sync(guild=THRESHOLD_GUILD_ID)
 
 intents = discord.Intents.default()
 the_actual_revealer_bot = RevealerBotDiscordClient(intents=intents)
